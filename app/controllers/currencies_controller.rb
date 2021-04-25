011a073992e4fda25c48a800baeae2373bfd06a4
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-04-25 23:59:44 +0000

    fin_part1

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,6 +4,10 @@ class CurrenciesController < ApplicationController
     @raw_data = open("https://api.exchangerate.host/symbols").read
     #@array_of_symboles
    render({ :template => "currency_templates/step_one.html.erb"})
+   @parsed_data = JSON.parse(@raw_data)
+   @symbols_hash = @parsed_data.fetch("symbols")
+   @array_of_symbols = @symbols_hash.keys
+
   end
 
 end
