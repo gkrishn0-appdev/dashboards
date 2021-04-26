f1a788873caae3e39a1f3fc3fe33938e898634d5
gkrishn0 <gkrishn0@chicagobooth.edu>
2021-04-26 06:49:39 +0000

    next

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -8,5 +8,16 @@ class CurrenciesController < ApplicationController
   
 
   end
+    def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+   @symbols_hash = @parsed_data.fetch("symbols")
+   @array_of_symbols = @symbols_hash.keys
+   @first = params.fetch("from_currency")
+   #render({ :template => "currency_templates/step_one.html.erb"})
+   render({ :template => "currency_templates/step_two.html.erb"})
+  
+
+  end
 
 end
